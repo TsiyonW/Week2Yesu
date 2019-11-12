e5358f969cb593780e5bf3c146e75eb1a875ba22
Tsiyon <tsiyonwul.tw@gmail.com>
2019-11-12 09:38:44 +0300

    Practice js implementation added

--- pracJS.js
+++ pracJS.js
@@ -1,1 +1,12 @@
+var z = x+y;
+alert(z);   //will alert NaN since we havent given a value to x and y
 
+var x = 25;
+var y = 2;
+x = x*y;
+console.log(x);
+var car = ["Volvo", "Jeep", "Mercedes"]
+car[0] = "Ford"
+for(let i; i<10;i++){
+    //
+}
